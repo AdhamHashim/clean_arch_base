714d7c9d0ddbe490142e4d332edc7824dc97f923
ahmedsaleh210 <ahmed.saleh212020@gmail.com>
2024-03-22 23:13:59 +0200

    Remove unused import in home presentation imports

--- lib/src/features/home /presentation/imports/presentaion_imports.dart
+++ lib/src/features/home /presentation/imports/presentaion_imports.dart
@@ -2,11 +2,10 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_base/src/config/res/color_manager.dart';
 import 'package:flutter_base/src/config/res/constans_manager.dart';
-import 'package:flutter_base/src/core/helpers/custom_widget_validator.dart';
 import 'package:flutter_base/src/core/shared/base_state.dart';
 import 'package:flutter_base/src/core/widgets/custom_loading.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
+
 import '../../domain/imports/domain_imports.dart';
 
 part '../cubit/home_cubit.dart';
