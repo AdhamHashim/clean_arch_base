04e3d22d9ecdf4762a8c2cfbb3fb3cb3e8ac0d65
ahmedsaleh210 <ahmed.saleh212020@gmail.com>
2024-03-22 23:13:47 +0200

    Remove unnecessary imports

--- lib/src/core/helpers/validators.dart
+++ lib/src/core/helpers/validators.dart
@@ -1,6 +1,4 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 
 import '../../config/language/locale_keys.g.dart';
 
