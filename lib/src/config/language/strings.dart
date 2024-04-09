8234d5de7327952c8c177b2edc5696a80432bf97
ceto2016 <ceto2016@gmail.com>
2024-04-09 06:03:39 +0200

    add strings generator

---
+++ lib/src/config/language/strings.dart
@@ -0,0 +1,106 @@
+import 'package:easy_localization/easy_localization.dart';
+
+abstract class Strings {
+  static const String _passValidation = 'passValidation';
+  static String get passValidation => _passValidation.tr();
+
+  static const String _passRequiredValidation = 'passRequiredValidation';
+  static String get passRequiredValidation => _passRequiredValidation.tr();
+
+  static const String _mailValidation = 'mailValidation';
+  static String get mailValidation => _mailValidation.tr();
+
+  static const String _phoneValidation = 'phoneValidation';
+  static String get phoneValidation => _phoneValidation.tr();
+
+  static const String _confirmValidation = 'confirmValidation';
+  static String get confirmValidation => _confirmValidation.tr();
+
+  static const String _fillField = 'fillField';
+  static String get fillField => _fillField.tr();
+
+  static const String _operationFaild = 'operationFaild';
+  static String get operationFaild => _operationFaild.tr();
+
+  static const String _progressFaild = 'progressFaild';
+  static String get progressFaild => _progressFaild.tr();
+
+  static const String _intenetWeaness = 'intenetWeaness';
+  static String get intenetWeaness => _intenetWeaness.tr();
+
+  static const String _serverError = 'serverError';
+  static String get serverError => _serverError.tr();
+
+  static const String _checkInternet = 'checkInternet';
+  static String get checkInternet => _checkInternet.tr();
+
+  static const String _errorExeptionNoconnection = 'errorExeptionNoconnection';
+  static String get errorExeptionNoconnection => _errorExeptionNoconnection.tr();
+
+  static const String _errorexeptionNointernetdesc = 'errorExeption_NointernetDesc';
+  static String get errorexeptionNointernetdesc => _errorexeptionNointernetdesc.tr();
+
+  static const String _errorexeptionNotcontain = 'errorExeption_NotContain';
+  static String get errorexeptionNotcontain => _errorexeptionNotcontain.tr();
+
+  static const String _errorexeptionNotcontaindesc = 'errorExeption_notContainDesc';
+  static String get errorexeptionNotcontaindesc => _errorexeptionNotcontaindesc.tr();
+
+  static const String _visitorText = 'visitorText';
+  static String get visitorText => _visitorText.tr();
+
+  static const String _expetion = 'expetion';
+  static String get expetion => _expetion.tr();
+
+  static const String _contactUs = 'contact_us';
+  static String get contactUs => _contactUs.tr();
+
+  static const String _cancele = 'cancele';
+  static String get cancele => _cancele.tr();
+
+  static const String _photoLibrary = 'PhotoLibrary';
+  static String get photoLibrary => _photoLibrary.tr();
+
+  static const String _camera = 'Camera';
+  static String get camera => _camera.tr();
+
+  static const String _error = 'error';
+  static String get error => _error.tr();
+
+  static const String _badRequest = 'bad_request';
+  static String get badRequest => _badRequest.tr();
+
+  static const String _unauthorized = 'unauthorized';
+  static String get unauthorized => _unauthorized.tr();
+
+  static const String _notFound = 'notFound';
+  static String get notFound => _notFound.tr();
+
+  static const String _name = 'name';
+  static String get name => _name.tr();
+
+  static const String _continueAsA = 'continueAsA';
+  static String get continueAsA => _continueAsA.tr();
+
+  static const String _email = 'email';
+  static String get email => _email.tr();
+
+  static const String _signIn = 'sign_in';
+  static String get signIn => _signIn.tr();
+
+  static const String _welcome = 'welcome';
+  static String get welcome => _welcome.tr();
+
+  static const String _register = 'register';
+  static String get register => _register.tr();
+
+  static const String _pleaseEnterTheCodeSentTo = 'please_enter_the_code_sent_to';
+  static String get pleaseEnterTheCodeSentTo => _pleaseEnterTheCodeSentTo.tr();
+
+  static const String _shortKey = 'short_key';
+  static String get shortKey => _shortKey.tr();
+
+  static const String _shortKey2 = 'short_key2';
+  static String get shortKey2 => _shortKey2.tr();
+
+}
