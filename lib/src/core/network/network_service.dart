5f0cec473d3f4661a03334f318fde1ef9aa28d78
ahmedsaleh210 <ahmed.saleh212020@gmail.com>
2024-06-24 13:55:19 +0300

    Refactor NetworkService to use interface instead of abstract class

--- lib/src/core/network/network_service.dart
+++ lib/src/core/network/network_service.dart
@@ -1,6 +1,6 @@
 import 'network_request.dart';
 
-abstract class NetworkService {
+abstract interface class NetworkService {
   Future<Model> callApi<Model>(
     NetworkRequest networkRequest, {
     Model Function(dynamic json)? mapper,
